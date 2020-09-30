a429c7bc16e260d98807eb14a9192d729d47714f
alesandra mempis <alesandramempis@gmail.com>
2020-09-30 14:00:25 -0300

    Classes do painel e vida

---
+++ script-python/teste02.py
@@ -0,0 +1,8 @@
+contador = 0
+while (contador <100):
+        print(contador)
+        contador = contador+1
+        if(contador%2):
+                print('Números são ímpares =', contador);
+        else:
+                print('Números são pares =', contador);
