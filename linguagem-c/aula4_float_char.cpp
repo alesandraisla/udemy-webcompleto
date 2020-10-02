945e4c304c68d4e48c061ef4de37b136cffeb310
alesandra mempis <alesandramempis@gmail.com>
2020-10-02 14:08:14 -0300

    Utilizando float e char

---
+++ linguagem-c/aula4_float_char.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+	
+	int inteira = 10;       //Usa o comando %i
+	float decimal = 10.5;   //Usa o comando %f para ajustar as casa decimais colocar %0.2f
+	char caractere = 'a';   // Usa o comando %c
+	
+	printf ("%i\n", inteira);
+	printf("%0.2f\n", decimal);
+	printf("%c\n", caractere);	
+	
+	
+	return 0;
+}
