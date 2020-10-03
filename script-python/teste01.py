9835b4e1f8d7f2be7dc27c1892c9c966b9836ef2
alesandra mempis <alesandramempis@gmail.com>
2020-10-02 22:11:47 -0300

    exclusao de arquivos

--- script-python/teste01.py
+++
@@ -1,4 +0,0 @@
-nome = input ("Qual é o seu nome?")
-idade = input ("Qual é a sua idade?")
-peso = input ("Qual é o seu peso?")
-print (nome, idade, peso)
--- script-python/teste02.py
+++
@@ -1,8 +0,0 @@
-contador = 0
-while (contador <100):
-        print(contador)
-        contador = contador+1
-        if(contador%2):
-                print('Números são ímpares =', contador);
-        else:
-                print('Números são pares =', contador);
